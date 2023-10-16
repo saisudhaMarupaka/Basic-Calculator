189347c45dc4715c47c5bcdbe43b403e7783aeee
saisudhaMarupaka <148038017+saisudhaMarupaka@users.noreply.github.com>
2023-10-16 12:47:46 +0530

    Delete Basic_projects directory

--- Basic_projects/ReverseWord.js
+++
@@ -1,6 +0,0 @@
-let sentence="Reverse of each word in a sentence";
-function reverseWords(str) {
-    let reverse = str.split(" ").map(word => word.split("").reverse().join(""));
-    return reverse.join(" ");
-  }
-  console.log(reverseWords(sentence));
